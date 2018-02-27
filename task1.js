d510e385f575d24708e0b003d88cd020438cd833
Katsiaryna Zakrzheuskaya <36811488+Katerinamel@users.noreply.github.com>
2018-02-27 16:13:26 +0300

    Update task1.js

--- task1.js
+++ task1.js
@@ -1,19 +1,10 @@
-var lines = 7;
-var str = " ";
-var simb = "*";
-function DrawTriangle () {
-if (lines > 2) 
-    {
-    for(var i = 0; i < lines; i++)
-    {str += simb;
-    console.log(str)}
-    }
-    else 
-    {
-        for(var i = 0; i < 3; i++)
-        {str += simb;
-        console.log(str)}
 
-    }
+function drawTriangle(simb, lines) {
+    var str = " "
+    if (lines < 3) { lines = 3 };
+    for (var i = 0; i < lines; i++) {
+    str += simb 
+    console.log(str)  }
+
 }
-DrawTriangle(simb, lines);
+drawTriangle("*", 10);
